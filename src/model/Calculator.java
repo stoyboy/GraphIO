07910c46398acf62e266e8fb0ba03bae86234d55
Stoyboy <jocas03@hotmail.com>
2022-05-29 05:14:26 +0200

    added calculateBridges implementations and updated calculateComponents

--- src/model/Calculator.java
+++ src/model/Calculator.java
@@ -114,7 +114,7 @@ public class Calculator {
         return pathMatrix;
     }
 
-    public static int calculateComponents(Integer[][] pathMatrix) {
+    public static ArrayList<String> calculateComponents(Integer[][] pathMatrix) {
         String[] rows = new String[pathMatrix.length];
         ArrayList<String> components = new ArrayList<>();
 
@@ -126,22 +126,45 @@ public class Calculator {
             rows[i] = tmp.toString();
         }
 
+        //sortiert doppelte zeilen aus
         for (String row : rows) {
             if (!components.contains(row))
                 components.add(row);
         }
 
-        return components.size();
+        for (String uniqueRow : components) {
+            StringBuilder str = new StringBuilder();
+            str.append('{');
+            for (int i = 0; i < uniqueRow.length(); i++) {
+                if (uniqueRow.charAt(i) == '1')
+                    str.append(convertIntToLetter(i)).append(uniqueRow.lastIndexOf('1') == i ? "" : ',');
+            }
+            str.append('}');
+            components.set(components.indexOf(uniqueRow), str.toString());
+        }
+
+        return components;
     }
 
-
+    public static void printComponents(ArrayList<String> components) {
+        for (int i = 0; i < components.size(); i++) {
+            String str = "K" +
+                    convertToSubscript(i + 1) +
+                    " = (K(" +
+                    (i + 1) +
+                    "), " +
+                    components.get(i) +
+                    ")";
+            System.out.println(str);
+        }
+    }
 
     public static String calculateArticulations(Matrix matrix, int componentCount) {
         ArrayList<Integer> articulationNodes = new ArrayList<>();
         Integer[][] adjacencyMatrix = duplicateMatrix(matrix.getMatrix());
         Integer[][] tmpAdjacencyMatrix = duplicateMatrix(adjacencyMatrix);
         int matrixSize = matrix.getMatrix().length;
-        int componentsCount;
+        int tmpComponentCount;
 
         for (int i = 0; i < matrixSize; i++) {
             for (int j = 0; j < tmpAdjacencyMatrix.length; j++) {
@@ -150,8 +173,8 @@ public class Calculator {
             }
 
             Integer[][] tmpPathMatrix = calculatePathmatrix(tmpAdjacencyMatrix);
-            componentsCount = calculateComponents(tmpPathMatrix);
-            if (componentsCount > componentCount + 1)
+            tmpComponentCount = calculateComponents(tmpPathMatrix).size();
+            if (tmpComponentCount > componentCount)
                 articulationNodes.add(i);
 
             tmpAdjacencyMatrix = duplicateMatrix(adjacencyMatrix);
@@ -170,6 +193,34 @@ public class Calculator {
         return str.toString();
     }
 
+    public static ArrayList<String[]> calculateBridges(Matrix m, int componentCount) {
+        ArrayList<String[]> bridges = new ArrayList<>();
+        Integer[][] adjacencyMatrix = duplicateMatrix(m.getMatrix());
+        Integer[][] tmpAdjacencyMatrix = duplicateMatrix(adjacencyMatrix);
+        int matrixSize = m.getMatrix().length;
+
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                if (adjacencyMatrix[i][j] == 1) {
+                    tmpAdjacencyMatrix[i][j] = 0;
+                    Integer[][] tmpPathMatrix = calculatePathmatrix(tmpAdjacencyMatrix);
+                    int tmpComponentCount = calculateComponents(tmpPathMatrix).size();
+                    if (tmpComponentCount > componentCount) {
+                        bridges.add(new String[]{convertIntToLetter(i), convertIntToLetter(j)});
+                    }
+                    tmpAdjacencyMatrix = duplicateMatrix(adjacencyMatrix);
+                }
+            }
+        }
+        return bridges;
+    }
+
+    public static void printBridges(ArrayList<String[]> bridges) {
+        for (String[] bridge: bridges) {
+            System.out.println("["+bridge[0]+","+bridge[1]+"]");
+        }
+    }
+
     private static String convertIntToLetter(int i) {
         StringBuilder letters = new StringBuilder();
         int quot = i / 26;
@@ -181,6 +232,17 @@ public class Calculator {
         } else {
             return convertIntToLetter(quot-1) + letters;
         }
+    }
+
+    private static String convertToSubscript(int i) {
+        String num = Integer.toString(i);
+        StringBuilder letters = new StringBuilder();
+
+        for (int j = 0; j < num.length(); j++) {
+            letters.append((char) (0x2080 + Integer.parseInt(Character.toString(num.charAt(j)))));
+        }
+
+        return letters.toString();
     }
 
     private static Integer[][] duplicateMatrix(Integer[][] matrix) {
